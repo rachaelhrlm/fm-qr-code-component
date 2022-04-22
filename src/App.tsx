ffb9a84e98e9d83b14205d9e3878cc8aa12d7824
Rachael Morris <rachael.morris@sky.uk>
2022-04-22 20:18:00 +0100

    update styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export const App: FunctionComponent = () => {
     <main className="bg-gray-light h-screen w-full flex place-content-center place-items-center p-8">
       <div className="bg-white p-4 border rounded-3xl w-[22rem] shadow-xl shadow-black/5">
         <img className="border rounded-2xl" src={`${process.env.PUBLIC_URL}/images/image-qr-code.png`} alt="QR code" />
-        <div className="text-center space-y-6 p-5">
+        <div className="text-center space-y-5 p-5">
           <h1 className="font-bold tracking-wide leading-tight text-[1.4rem] text-blue-dark">Improve your front-end skills by building projects</h1>
           <p className="text-gray-blue">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
