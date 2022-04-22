1b123ccaa014f8eea6e6b513985e58fa0ebd0319
Rachael Morris <rachael.morris@sky.uk>
2022-04-22 19:05:46 +0100

    update font

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import React, { FunctionComponent } from "react";
 export const App: FunctionComponent = () => {
   return (
     <main className="bg-gray-light h-screen w-full flex place-content-center place-items-center p-8">
-      <div className="flex flex-col bg-white p-5 border rounded-3xl w-[24rem]">
+      <div className="flex flex-col bg-white p-5 border rounded-3xl w-[22rem]">
         <img className="border rounded-2xl" src={`${process.env.PUBLIC_URL}/images/image-qr-code.png`} alt="QR code" />
-        <div className="flex flex-col text-center gap-6 p-7">
-          <h1 className="text-2xl text-blue-dark font-bold">Improve your front-end skills by building projects</h1>
+        <div className="flex flex-col text-center gap-6 p-5">
+          <h1 className="font-bold tracking-wide leading-tight text-[1.4rem] text-blue-dark">Improve your front-end skills by building projects</h1>
           <p className="text-gray-blue">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
       </div>
