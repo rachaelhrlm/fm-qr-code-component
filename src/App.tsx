2235b1fe7e2ce76de42be60957416b73e4089cfd
Rachael Morris <rachael.morris@sky.uk>
2022-04-22 17:55:46 +0100

    add main tag

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from "react";
 
 export const App: FunctionComponent = () => {
   return (
-    <div className="bg-gray-light h-screen w-full flex place-content-center place-items-center p-8">
+    <main className="bg-gray-light h-screen w-full flex place-content-center place-items-center p-8">
       <div className="flex flex-col bg-white p-5 border rounded-3xl w-[24rem]">
         <img className="border rounded-2xl" src={`${process.env.PUBLIC_URL}/images/image-qr-code.png`} alt="QR code" />
         <div className="flex flex-col text-center gap-6 p-7">
@@ -10,6 +10,6 @@ export const App: FunctionComponent = () => {
           <p className="text-gray-blue">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
