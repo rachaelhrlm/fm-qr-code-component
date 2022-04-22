9a4b372dda500fc8da1f2bd1c80986a4d3facb2f
Rachael Morris <rachael.morris@sky.uk>
2022-04-22 17:12:01 +0100

    create qr code component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import React, { FunctionComponent } from "react";
 
 export const App: FunctionComponent = () => {
-  return <div></div>;
+  return (
+    <div className="bg-gray-light h-screen w-full flex place-content-center place-items-center p-8">
+      <div className="flex flex-col bg-white p-5 border rounded-3xl w-[24rem]">
+        <img className="border rounded-2xl" src={`${process.env.PUBLIC_URL}/images/image-qr-code.png`} alt="QR code" />
+        <div className="flex flex-col text-center gap-6 p-7">
+          <h1 className="text-2xl text-blue-dark font-bold">Improve your front-end skills by building projects</h1>
+          <p className="text-gray-blue">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
+    </div>
+  );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          dark: "hsl(218, 44%, 22%)",
+        },
+        gray: {
+          light: "hsl(212, 45%, 89%)",
+          blue: "hsl(220, 15%, 55%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
